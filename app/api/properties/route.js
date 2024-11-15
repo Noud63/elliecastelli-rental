8b02ea9a10b881097b695cbc0643decd2367340b
Noud63 <noudvandun@gmail.com>
2024-11-15 16:40:42 +0100

    *

--- app/api/properties/route.js
+++ app/api/properties/route.js
@@ -3,6 +3,7 @@ import Property from "@/models/Property"
 import { getSessionUser } from "@/utils/getSessionUser"
 import cloudinary from "@/config/cloudinary"
 
+
 // Get /api/properties
 export const GET = async (request) => {
    try {
